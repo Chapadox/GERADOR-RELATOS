428c10c9d71f05002ead3d0f4984001a2cbc7355
Chapadox <ffcxcfsantos2@gmail.com>
2023-05-24 15:37:01 -0300

    Gerador de Relatos V1.0

--- gerador.js
+++ gerador.js
@@ -10,31 +10,144 @@ const data = new Date()
 const mes = data.getMonth() + 1
 const dia = data.getDate()
 
-function relaSemConexao () {
-     prompt.question('Qual seu ATT ? ', resposta => {
-
-        const relato = [` 
-CLIENTE INFORMA ESTAR SEM CONEXÃO.
-PROBLEMA TEVE INICIO: ${dia + '/' + mes}
-FEITO OS PROCEDIMENTOS BASICOS SEM NORMALIZAÇÃO.
-APRESENTA LED LOS.
-POSSUI CONEXÃO CABEADA TAMBEM ACESSO.
-VERIFICADO OS CABOS.
-STATUS: OFFLINE
-MENSAGEM AO SE CONECTAR NA REDE WI-FI: INTERNET PODE ESTAR INDISPONIVEL.
-DISPONIBILIDADE PARA VISITA: LIGAR ANTES.
-CIENTE DO PRAZO.
-ATT: ${resposta.toUpperCase()}`] 
-
-
-
-
-        fs.writeFile('relato.txt', relato[0], (err) => {
-            if (err) console.log(err);
-            console.log('salvo')
-            prompt.close()
+const pergunta1 = () => {
+     return new Promise((resolve, reject) => {
+        prompt.question('Os equipamento foram reiniciados ? ', (res) => {
+            if (res == 'sim' || res == 'Sim') {
+                resolve()
+            } else {
+                console.log('Reinicie o equipamento junto ao cliente');
+                prompt.close()
+                reject();
+            }
         })
-     })
+    })
+}
+
+const pergunta2 = () => {
+    return new Promise((resolve, reject) => {
+       prompt.question('Apresenta led los ? ', (res) => {
+           if (res == 'sim' || res == 'Sim') {
+                fs.appendFileSync('./teste.txt','\nAPRESENTA LED LOS', (err) => {
+                    console.log(err);
+                })
+            resolve()
+           } else {
+            fs.appendFileSync('./teste.txt','\nNÃO APRESENTA LED LOS', (err) => {
+                console.log(err);
+            })
+            resolve()
+           }
+       })
+   })
+}
+
+const pergunta3 = () => {
+    return new Promise((resolve, reject) => {
+       prompt.question('Possui conexão cabeada ? ', (res) => {
+           if (res == 'sim' || res == 'Sim') {
+                fs.appendFileSync('./teste.txt','\nPOSSUI CONEXÃO CABEADA', (err) => {
+                    console.log(err);
+                })
+            resolve()
+           } else {
+                fs.appendFileSync('./teste.txt','\nNÃO POSSUI CONEXÃO CABEADA', (err) => {
+                    console.log('err');
+                })
+            resolve()
+           } 
+       })
+   })
+}
+
+const pergunta4 = () => {
+    return new Promise((resolve, reject) => {
+       prompt.question('Todos os cabos foram verificados ? ', (res) => {
+           if (res == 'sim' || res == 'Sim') {
+                fs.appendFileSync('./teste.txt','\nVERIFICADO OS CABOS', (err) => {
+                    console.log(err);
+                })
+            resolve()
+           } if (res == 'Não' || res == 'não') {
+                console.log('Por favor verifique o cabeamento dos equipamentos')
+            resolve()
+           } else {
+            reject('err');
+           }
+       })
+   })
+}
+
+const pergunta5 = () => {
+    return new Promise((resolve, reject) => {
+       prompt.question('Qual mensagem aparece ao se conectar na rede wi-fi ? ', (res) => {
+        fs.appendFileSync('./teste.txt', `\nMENSAGEM AO SE CONECTAR NA REDE WI-FI: ${res}`)
+        resolve()
+       })
+   })
+}
+
+const pergunta6 = () => {
+    return new Promise((resolve, reject) => {
+       prompt.question('Qual Melhor dia/horario para Visita tecnica ? ', (res) => {
+        fs.appendFileSync('./teste.txt', `\nDISPONIBILIDADE PARA VISITA: ${res}`)
+        resolve()
+       })
+   })
+}
+const pergunta7 = () => {
+    return new Promise((resolve, reject) => {
+       prompt.question('Qual status atual da rede ?  ', (res) => {
+        fs.appendFileSync('./teste.txt', `\nSTATUS: ${res}\n`)
+        resolve()
+       })
+   })
+}
+
+const pergunta8 = () => {
+    return new Promise((resolve, reject) => {
+       prompt.question('Alguma outra observação ?  ', (res) => {
+       switch (res) {
+        case 'sim': 
+            prompt.question('Digite o complemento: ', res => {
+                fs.appendFileSync('./teste.txt', res)
+                resolve()
+            })
+            break;
+        default: break;
+       }
+        
+       })
+   })
+}
+
+
+const pergunta9 = () => {
+    return new Promise((resolve, reject) => {
+       prompt.question('ATT ? ', (res) => {
+        fs.appendFileSync('./teste.txt', `\nATT: ${res}`)
+        resolve()
+       })
+   })
+}
+
+const relatoFinal = async () => {
+    fs.appendFileSync('./teste.txt', 
+    `CLIENTE INFORMA ESTAR SEM CONEXÃO
+PROBLEMA TEVE INICIO: ${dia + '/' + mes}`)
+    await pergunta1()
+    await pergunta2()
+    await pergunta3()
+    await pergunta4()
+    await pergunta5()
+    await pergunta6()
+    await pergunta7()
+    await pergunta8()
+    await pergunta9()
+    for (let i=0; i <3; i++){
+        console.log('GERANDO RELATO.....')
+    }
+    prompt.close()
 }
 
  prompt.question(` Gerador de Relatos v1.0
@@ -44,11 +157,9 @@ ATT: ${resposta.toUpperCase()}`]
  Selecione uma opção: `, resposta => {
     switch (resposta) {
         case '1':
-            relaSemConexao()
+            relatoFinal()
             break;
         default: 
             console.log('default')
     }
 })
-
-
